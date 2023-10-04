d37d8cb867857df627b1c203183491e1eb94f20a
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-10-04 16:39:28 +0800

    feat: SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 </script>
 
 <svelte:head>
+	<title>Kost Darmo Surabaya. Harga terjangkau, bersih, cocok untuk pekerja</title>
+	<meta name="description" content="Kost Darmo dengan tarif yang terjangkau, merupakan pilihan sempurna bagi para pekerja yang berlokasi di Surabaya Barat. Harga mulai dari Rp 850.000,- per bulan!" />
 	<meta name="robots" content="index, follow" />
 	<script src="https://kit.fontawesome.com/823d655421.js" crossorigin="anonymous"></script>
 </svelte:head>
