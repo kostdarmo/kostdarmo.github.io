7707212c41341d8338893d216b848136cab79a2e
Ivana Setiawan <ivana.setiawan@gmail.com>
2023-10-04 16:47:23 +0800

    feat: SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 <svelte:head>
 	<title>Kost Darmo Surabaya. Harga terjangkau, bersih, cocok untuk pekerja</title>
 	<meta name="description" content="Kost Darmo dengan tarif yang terjangkau, merupakan pilihan sempurna bagi para pekerja yang berlokasi di Surabaya Barat. Harga mulai dari Rp 850.000,- per bulan!" />
+	<meta name="keywords" content="kos, sewa kamar, kost, kost2an, surabaya, surabaya barat, murah, harga terjangkau, bersih, khusus wanita, khusus pekerja, wifi, darmo, indah, darmo indah timur, tandes, tandes lor, harian, bulanan, pakai AC" />
 	<meta name="robots" content="index, follow" />
 	<script src="https://kit.fontawesome.com/823d655421.js" crossorigin="anonymous"></script>
 </svelte:head>
